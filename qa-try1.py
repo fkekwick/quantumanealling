048b6744e0854e582380bb0abef3c4f2de8f521f
fkekwick <96203075+fkekwick@users.noreply.github.com>
2021-12-16 02:20:59 +0000

    Update qa-try1.py

--- qa-try1.py
+++ qa-try1.py
@@ -10,7 +10,7 @@ current_time = now.strftime("%m-%d-%H.%M.%S")
 
 #start code to connect to d wave
 ##################################################
-#sampler = DWaveSampler(solver='DW_2000Q_6',token='DEV-76a26fe9d6b5acd4e67793b0987934a72275b6a6') # 2000Q solver,
+sampler = DWaveSampler(solver='DW_2000Q_6',token=) # 2000Q solver,
 
 #####################################################################################################################
 print("Connected to sampler", sampler.solver.name)
@@ -19,35 +19,36 @@ print("Connected to sampler", sampler.solver.name)
 
 
 #embedding
-# from helpers.embedding import DirectEmbeddingComposite
-# sampler_embedded = DirectEmbeddingComposite(sampler)
 
 from dwave.system.composites import EmbeddingComposite
 sampler_embedded = EmbeddingComposite(sampler)
 
-#encode h and J
+#encode h and J using Ising model
+
 J = { (0,0):0, (0,1):1, (0,2):1, (0,3):0, (0,4):0,
 (1,2):1, (1,4):1,
-(2,3):1, (2,4):1 }    # should these be 1 or -1???
+(2,3):1, (2,4):1 }   
 
-h = [-1.5,-2.5,-3.5,-0.5,-1.5]  #k can vary but here k = 0.5?
+h = [-1.5,-2.5,-3.5,-0.5,-1.5]  # h is a function of k, here k = 0.5
 
 
-##code for annealing
+#code for annealing
 runs= 100   #no. runs
 results = sampler_embedded.sample_ising(h, J, 
     num_reads=runs,
-    annealing_time=200)
+    annealing_time=200)  #vary annealing time
 
 print("QPU time used:", results.info['timing']['qpu_access_time'], "microseconds.")
 print(results)
-# print(sampleset.info["timing"]) 
-# print(sampleset.info['problem_id']) 
-# print(sampleset.info['embedding_context'])
-# print(sampleset.info['warnings'])
-energy = results.record.energy
-sample = results.record.sample.tolist()   ##to list???sample.tolist()
-num_occurrences= results.record.num_occurrences
+print(sampleset.info["timing"]) 
+print(sampleset.info['problem_id']) 
+print(sampleset.info['embedding_context'])
+print(sampleset.info['warnings'])
+
+
+energy = results.record.energy  #record energy of each sample
+sample = results.record.sample.tolist()    #record state of each sample
+num_occurrences= results.record.num_occurrences #record number of occurences of each sample
 s = results.record.sample
 
 
@@ -69,6 +70,8 @@ filename= 'DwaveDataOut/' +filename # file name plus path
 f=open(filename,'w') # open the file to write
 f.write(str(bundledict)) # write information
 f.close() # close file
+
+#record results in a pandas data file also 
 
 PAND = results.to_pandas_dataframe(sample_column=True)  
 filename1='PANDAS-outputdata-'+'QAtest-' + current_time +'.txt' # file name
